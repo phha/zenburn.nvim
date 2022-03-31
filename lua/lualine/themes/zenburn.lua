caf166be22b57d8ab3d1fb026894d04f83d3d29a
Philipp Hack <philipp.hack@gmail.com>
2022-03-31 14:41:20 +0200

    Added lualine theme

---
+++ lua/lualine/themes/zenburn.lua
@@ -0,0 +1,32 @@
+return {
+  normal = {
+    a = {bg ="#2f2f2f", fg="#dcdcdc", gui="bold"},
+    b = {bg ="#383838", fg="#dcdcdc"},
+    c = {bg ="#434443", fg="#dcdcdc"},
+  },
+  insert = {
+    a = {bg ="#dca3a3", fg="#2f2f2f", gui="bold"},
+    b = {bg ="#434443", fg="#dcdcdc"},
+    c = {bg ="#484848", fg="#dcdcdc"},
+  },
+  visual = {
+    a = {bg ="#ffcfaf", fg="#2f2f2f", gui="bold"},
+    b = {bg ="#434443", fg="#dcdcdc"},
+    c = {bg ="#484848", fg="#dcdcdc"},
+  },
+  replace = {
+    a = {bg ="#8cd0d3", fg="#2f2f2f", gui="bold"},
+    b = {bg ="#434443", fg="#dcdcdc"},
+    c = {bg ="#484848", fg="#dcdcdc"},
+  },
+  command = {
+    a = {bg ="#efef8f", fg="#2f2f2f", gui="bold"},
+    b = {bg ="#434443", fg="#dcdcdc"},
+    c = {bg ="#484848", fg="#dcdcdc"},
+  },
+  inactive = {
+    a = {bg ="#484848", fg="#2f2f2f", gui="bold"},
+    b = {bg ="#484848", fg="#dcdcdc"},
+    c = {bg ="#484848", fg="#dcdcdc"},
+  },
+}
