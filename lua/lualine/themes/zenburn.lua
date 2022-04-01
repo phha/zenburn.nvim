47665ca0c6bd871f36eebc6007c118b74d3b3bf8
Philipp Hack <philipp.hack@gmail.com>
2022-04-01 11:03:09 +0200

    lualine: use references to core palette

--- lua/lualine/themes/zenburn.lua
+++ lua/lualine/themes/zenburn.lua
@@ -1,32 +1,33 @@
+local c = require("zenburn.palette")
 return {
-  normal = {
-    a = {bg ="#2f2f2f", fg="#dcdcdc", gui="bold"},
-    b = {bg ="#383838", fg="#dcdcdc"},
-    c = {bg ="#434443", fg="#dcdcdc"},
-  },
-  insert = {
-    a = {bg ="#dca3a3", fg="#2f2f2f", gui="bold"},
-    b = {bg ="#434443", fg="#dcdcdc"},
-    c = {bg ="#484848", fg="#dcdcdc"},
-  },
-  visual = {
-    a = {bg ="#ffcfaf", fg="#2f2f2f", gui="bold"},
-    b = {bg ="#434443", fg="#dcdcdc"},
-    c = {bg ="#484848", fg="#dcdcdc"},
-  },
-  replace = {
-    a = {bg ="#8cd0d3", fg="#2f2f2f", gui="bold"},
-    b = {bg ="#434443", fg="#dcdcdc"},
-    c = {bg ="#484848", fg="#dcdcdc"},
-  },
-  command = {
-    a = {bg ="#efef8f", fg="#2f2f2f", gui="bold"},
-    b = {bg ="#434443", fg="#dcdcdc"},
-    c = {bg ="#484848", fg="#dcdcdc"},
-  },
-  inactive = {
-    a = {bg ="#484848", fg="#2f2f2f", gui="bold"},
-    b = {bg ="#484848", fg="#dcdcdc"},
-    c = {bg ="#484848", fg="#dcdcdc"},
-  },
+	normal = {
+		a = {bg =c.SpecialComment.fg, fg=c.SignColumn.fg, gui="bold"},
+		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+	},
+	insert = {
+		a = {bg =c.Function.fg, fg=c.SignColumn.fg, gui="bold"},
+		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+	},
+	visual = {
+		a = {bg =c.Define.fg, fg=c.SignColumn.fg, gui="bold"},
+		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+	},
+	replace = {
+		a = {bg =c.Number.fg, fg=c.SignColumn.fg, gui="bold"},
+		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+	},
+	command = {
+		a = {bg =c.Boolean.fg, fg=c.SignColumn.fg, gui="bold"},
+		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+	},
+	inactive = {
+		a = {bg =c.Pmenu.fg, fg=c.SignColumn.fg, gui="bold"},
+		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+	},
 }
