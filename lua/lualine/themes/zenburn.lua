267d11e78eb8eda275075be35fa82fc08c5487d5
Philipp Hack <philipp.hack@gmail.com>
2022-04-01 13:32:07 +0200

    prettier lualine

--- lua/lualine/themes/zenburn.lua
+++ lua/lualine/themes/zenburn.lua
@@ -2,32 +2,38 @@ local c = require("zenburn.palette")
 return {
 	normal = {
 		a = {bg =c.SpecialComment.fg, fg=c.SignColumn.fg, gui="bold"},
-		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		b = {bg =c.LineNr.fg, fg=c.Normal.fg},
 		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+		z = {bg =c.Delimiter.fg, fg=c.SignColumn.fg},
 	},
 	insert = {
 		a = {bg =c.Function.fg, fg=c.SignColumn.fg, gui="bold"},
-		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		b = {bg =c.LineNr.fg, fg=c.Normal.fg},
 		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+		z = {bg =c.Delimiter.fg, fg=c.SignColumn.fg},
 	},
 	visual = {
 		a = {bg =c.Define.fg, fg=c.SignColumn.fg, gui="bold"},
-		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		b = {bg =c.LineNr.fg, fg=c.Normal.fg},
 		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+		z = {bg =c.Delimiter.fg, fg=c.SignColumn.fg},
 	},
 	replace = {
 		a = {bg =c.Number.fg, fg=c.SignColumn.fg, gui="bold"},
-		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		b = {bg =c.LineNr.fg, fg=c.Normal.fg},
 		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+		z = {bg =c.Delimiter.fg, fg=c.SignColumn.fg},
 	},
 	command = {
 		a = {bg =c.Boolean.fg, fg=c.SignColumn.fg, gui="bold"},
-		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		b = {bg =c.LineNr.fg, fg=c.Normal.fg},
 		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+		z = {bg =c.Delimiter.fg, fg=c.SignColumn.fg},
 	},
 	inactive = {
-		a = {bg =c.Pmenu.fg, fg=c.SignColumn.fg, gui="bold"},
-		b = {bg =c.LineNr.fg, fg=c.LineNr.bg},
+		a = {bg =c.Delimiter.fg, fg=c.SignColumn.fg, gui="bold"},
+		b = {bg =c.LineNr.fg, fg=c.Normal.fg},
 		c = {bg =c.CursorLine.bg, fg=c.Normal.fg},
+		z = {bg =c.Delimiter.fg, fg=c.SignColumn.fg},
 	},
 }
