28d92c62acbdd9ee5b9e3bfccb4ea146de0c2df0
Philipp Hack <philipp.hack@gmail.com>
2022-11-17 11:45:14 +0100

    Less jarring gitsigns line highlights

--- lua/zenburn/highlights/gitsigns.lua
+++ lua/zenburn/highlights/gitsigns.lua
@@ -2,11 +2,11 @@ local c = require("zenburn.palette")
 return {
     GitSignsAdd = { fg=c.Comment.fg },
     GitSignsAddNr = { fg=c.Comment.fg },
-    GitSignsAddLn = { bg=c.Comment.fg },
+    GitSignsAddLn = { bg=c.DiffAdd.bg },
     GitSignsChange = { fg=c.Label.fg },
     GitSignsChangeNr = { fg=c.Label.fg },
-    GitSignsChangeLn = { bg=c.Label.fg },
+    GitSignsChangeLn = { bg=c.DiffChange.bg },
     GitSignsDelete = { fg=c.String.fg },
     GitSignsDeleteNr = { fg=c.String.fg },
-    GitSignsDeleteLn = { bg=c.String.fg },
+    GitSignsDeleteLn = { bg=c.DiffText.bg },
 }
